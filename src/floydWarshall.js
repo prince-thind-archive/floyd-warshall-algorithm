d39f77e1caf5f517c73e1c87cb46d29b75970093
Prince Thind <princethind799@gmail.com>
2021-07-12 19:53:19 +0530

    crude implementation of floyd-warshall

--- src/floydWarshall.js
+++ src/floydWarshall.js
@@ -1,7 +1,23 @@
 const floydWarhsall = function (arr) {
-    const res=arr;
-    
-    return res;
+  const res = [];
+
+  for (let temparr of arr) {
+    res.push(temparr.slice());
+  }
+
+  const arrLength = arr.length;
+
+  for (let k = 0; k < arrLength; k++) {
+    for (let i = 0; i < arrLength; i++) {
+      for (let j = 0; j < arrLength; j++) {
+          if(i==k||j==k){
+              break;
+          }
+          res[i][j]=Math.min(res[k][j]+res[i][k],res[i][j])
+      }
+    }
+  }
+  return res;
 };
 
 export default floydWarhsall;
